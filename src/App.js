0c7e4f14f10737d888f3bb74a03d971c3faca3a1
Tyler Reitz <tdreitz@gmail.com>
2018-07-29 17:49:29 -0700

    Adds <Router /> and test /404 & /assessment routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from "react"
+import React, { Component, Fragment } from "react"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
 import "./App.css"
+import Assessment from "./components/assessment"
+import FourOhFour from "./components/404"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
+      <div className="App bg-light">
+        <h1>Welcome Robert Ferraro</h1>
+        <Router>
+          <Switch>
+            <Route path="/assessment" component={Assessment} />
+            <Route path="*" component={FourOhFour} />
+          </Switch>
+        </Router>
       </div>
     )
   }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
 import React from "react"
-import { shallow } from "enzyme"
-import App from "./App"
+import { mount, shallow } from "enzyme"
+import { MemoryRouter } from "react-router-dom"
 
-it("renders without crashing", () => {
-  const component = shallow(<App />)
-  expect(component.length).toBe(1)
+import App from "./App"
+import Assessment from "./components/assessment"
+import FourOhFour from "./components/404"
+
+describe("App", () => {
+  it("renders without crashing", () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.length).toBe(1)
+  })
+
+  describe("Router", () => {
+    const setup = route =>
+      mount(
+        <MemoryRouter initialEntries={[route]} initialIndex={0}>
+          <App />
+        </MemoryRouter>
+      )
+
+    describe("/*", () => {
+      it("invalid path should redirect to 404", () => {
+        const wrapper = setup("/random")
+
+        expect(wrapper.find(FourOhFour)).toHaveLength(1)
+      })
+
+      it("invalid path should not render Assessment", () => {
+        const wrapper = setup("/random")
+
+        expect(wrapper.find(Assessment)).toHaveLength(0)
+      })
+    })
+
+    describe("/assessment", () => {
+      it("it renders", () => {
+        const wrapper = setup("/assessment")
+
+        expect(wrapper.find(Assessment)).toHaveLength(1)
+      })
+
+      it("does not render a 404", () => {
+        const wrapper = setup("/assessment")
+
+        expect(wrapper.find(FourOhFour)).toHaveLength(0)
+      })
+    })
+  })
 })
