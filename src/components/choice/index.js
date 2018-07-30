1277c529a4458cdb2dbf5cc50056576993a82e62
Tyler Reitz <tdreitz@gmail.com>
2018-07-29 21:55:17 -0700

    Adds <Choice /> component & tests

---
+++ src/components/choice/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+const Choices = ({ idx, name, value, text }) => (
+  <div>
+    <input type="radio" name={name} value={value} id={`${name}-${idx}`} />
+    <label for={`${name}-${idx}`}>{text}</label>
+  </div>
+)
+
+Choices.propTypes = {
+  name: PropTypes.string.isRequired,
+  value: PropTypes.number.isRequired,
+  idx: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired
+}
+
+export default Choices
---
+++ src/components/choice/index.test.js
@@ -0,0 +1,79 @@
+import React from "react"
+import { shallow } from "enzyme"
+import checkPropTypes from "check-prop-types"
+
+import Choice from "./"
+
+describe("Choice", () => {
+  const props = {
+    name: "nameOfRadioGroup",
+    value: 2,
+    idx: 1,
+    text: "text of the choice"
+  }
+
+  describe("input[type='radio']", () => {
+    it("renders", () => {
+      const wrapper = shallow(<Choice {...props} />)
+      expect(wrapper.exists()).toBe(true)
+    })
+
+    it("doesn't throw with expected props", () => {
+      const propErrors = checkPropTypes(
+        Choice.propTypes,
+        props,
+        "prop",
+        Choice.name
+      )
+
+      expect(propErrors).toBeUndefined()
+    })
+
+    it("renders a radio button", () => {
+      const wrapper = shallow(<Choice {...props} />)
+      expect(wrapper.find("input[type='radio']").exists()).toBe(true)
+    })
+
+    it("associates the radio button with a group", () => {
+      const wrapper = shallow(<Choice {...props} />)
+      const radioButton = wrapper.find(`[name="${props.name}"]`)
+      expect(wrapper.find("input[type='radio']").props().name).toBe(props.name)
+    })
+
+    it("associates the radio button with a value", () => {
+      const wrapper = shallow(<Choice {...props} />)
+      const radioButton = wrapper.find("input[type='radio']")
+      expect(wrapper.find("input[type='radio']").props().value).toBe(
+        props.value
+      )
+    })
+
+    it("accociates the radio button with a label", () => {
+      const wrapper = shallow(<Choice {...props} />)
+      const radioButton = wrapper.find("input[type='radio']")
+      expect(wrapper.find("input[type='radio']").props().id).toBe(
+        `${props.name}-${props.idx}`
+      )
+    })
+  })
+
+  describe("label", () => {
+    it("renders", () => {
+      const wrapper = shallow(<Choice {...props} />)
+      expect(wrapper.find("label").exists()).toBe(true)
+    })
+
+    it("text equals input value", () => {
+      const wrapper = shallow(<Choice {...props} />)
+      expect(wrapper.find("label").text()).toBe(props.text)
+    })
+
+    it("associate the label with an input", () => {
+      const wrapper = shallow(<Choice {...props} />)
+      console.log(wrapper.find("label").props().for)
+      expect(wrapper.find("label").props().for).toBe(
+        `${props.name}-${props.idx}`
+      )
+    })
+  })
+})
