4b1ae5a7fdbc925bac9ca21251f114d110303f80
Tyler Reitz <tdreitz@gmail.com>
2018-07-30 00:50:28 -0700

    Adds styling

--- src/components/assessment/index.js
+++ src/components/assessment/index.js
@@ -6,14 +6,18 @@ import Stepper from "../stepper"
 import GoBack from "../goback"
 
 export default () => (
-  <div>
-    <GoBack currentStep={2} />
-    <Stepper current={2} total={4} />
+  <div className="card p-5 m-lg-5 assessment-border">
+    <div className="d-flex justify-content-between align-items-center mb-3">
+      <GoBack currentStep={2} />
+      <Stepper current={2} total={4} />
+    </div>
     <Questions
+      className="my-2"
       title="What is Lorem Ipsum…"
       question="Lorem ipsum is simply dummy text of the printing industry. Lorem Ipsum has been industry's standard dummy text ever since since the 1500s"
     />
     <Choices
+      className="my-3"
       choices={[
         {
           name: "some question",
